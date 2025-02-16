a1332b402411f7f0429152f0f680f29f3c06b302
yhddqq <yhdd.123@qq.com>
2025-02-16 22:22:59 +0800

    BookDao.java和bookserviceimpl三个功能返回值修改，之前都是返回true

--- ssm1/src/main/java/org/example/dao/BookDao.java
+++ ssm1/src/main/java/org/example/dao/BookDao.java
@@ -10,15 +10,15 @@ public interface BookDao {
 //    @Insert("insert into tbl_book values(null,#{type},#{name},#{description})")
     @Insert("insert into tbl_book (type,name,description) values(#{type},#{name},#{description})")
     @Result(property = "id", column = "ids")
-    void save(Book book);
+    int save(Book book);
 
     @Update("update tbl_book set type=#{type}, name=#{name}, description=#{description} where ids=#{id}")
     @Result(property = "id", column = "ids")
-    void update(Book book);
+    int update(Book book);
 
     @Delete("delete from tbl_book where ids = #{id}")
     @Result(property = "id", column = "ids")
-    void delete(Integer id);
+    int delete(Integer id);
 
     @Select("select * from tbl_book where ids = #{id}")
     @Result(property = "id", column = "ids")
